3167074b7fb88ff5fb2af9ba7e4dd06c37fae6b0
Denny Tek <tek.denny@gmail.com>
2016-02-26 18:36:37 -0500

    Add error_handlers module

---
+++ app/error_handlers.py
@@ -0,0 +1,9 @@
+import flask
+from flask import Blueprint, render_template
+
+
+blueprint = flask.Blueprint('error_handlers', __name__)
+
+@blueprint.app_errorhandler(404)
+def handleerror404(e):
+    return render_template('404.html'), 404
